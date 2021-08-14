372de259275861f8a8ddc7ede91bde33512f3465
GeronimoJohn <johnageronimo1@gmail.com>
2021-08-14 22:07:55 +1200

    test fetch api

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+
+const movieUrl = "https://reactnative.dev/movies.json";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [description, setDescription] = useState([]);
+  const [title, setTitle] = useState([]);
+
+  useEffect(() => {
+    async function fetchJsonAPI() {
+      fetch(movieUrl)
+        .then((response) => response.json())
+        .then((json) => {
+          setMovies(json.movies);
+          setTitle(json.title);
+          setDescription(json.description);
+        })
+        .catch((error) => alert(error))
+        .finally(() => setIsLoading(false));
+    }
+
+    fetchJsonAPI();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text> Open up App.js to start working on your app! </Text>{" "}
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+          <Text style={styles.title}>{title}</Text>
+          <View style={{ borderBottomWidth: 1, marginBottom: 12 }}></View>
+          <FlatList
+            data={movies}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={{ paddingBottom: 10 }}>
+                <Text style={styles.movieText}>
+                  {item.id}. {item.title}
+                  {item.releaseYear}
+                </Text>
+              </View>
+            )}
+          />
+          <Text style={styles.description}>{description}</Text>
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    marginTop: 48,
+  },
+  movieText: {
+    fontSize: 26,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 18,
+    fontWeight: "200",
+    color: "green",
   },
 });
